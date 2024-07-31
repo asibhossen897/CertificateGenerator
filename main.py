fc224617dd0511c75d97db7a512e4bb31d6bcd1e
Asib Hossen <dev.asib@proton.me>
2024-07-31 15:11:59 +0600

    formatted the file and changed code from deprecated *textsize* by *textbbox* method

--- main.py
+++ main.py
@@ -7,21 +7,26 @@ FONT_COLOR = "#FFFFFF"
 template = Image.open(r'template.png')
 WIDTH, HEIGHT = template.size
 
+
 def make_certificates(name):
-    '''Function to save certificates as a .png file'''
+    """Function to save certificates as a .png file"""
 
     image_source = Image.open(r'template.png')
     draw = ImageDraw.Draw(image_source)
 
-    # Finding the width and height of the text. 
-    name_width, name_height = draw.textsize(name, font=FONT_FILE)
+    # Finding the bounding box of the text.
+    # The 'textsize' method has been deprecated and replaced by the 'textbbox' method in newer versions of Pillow
+    bbox = draw.textbbox((0, 0), name, font=FONT_FILE)
+    name_width = bbox[2] - bbox[0]
+    name_height = bbox[3] - bbox[1]
 
     # Placing it in the center, then making some adjustments.
     draw.text(((WIDTH - name_width) / 2, (HEIGHT - name_height) / 2 - 30), name, fill=FONT_COLOR, font=FONT_FILE)
 
     # Saving the certificates in a different directory.
-    image_source.save("./out/" + name +".png")
-    print('Saving Certificate of:', name)        
+    image_source.save("./out/" + name + ".png")
+    print('Saving Certificate of:', name)
+
 
 if __name__ == "__main__":
 
@@ -30,4 +35,3 @@ if __name__ == "__main__":
         make_certificates(name)
 
     print(len(names), "certificates done.")
-
